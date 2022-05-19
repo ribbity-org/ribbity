35dc6f0267ff5e4da26cb0e2cd2fc36951b375b5
C. Titus Brown <titus@idyll.org>
2022-05-19 06:49:24 -0700

    automate test running, yay

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,44 @@
 "Basic tests of ribbity functionality."
 from pickle import load
 import os
+import tempfile
+import shutil
 
 import pytest
+
+from ribbity.main_pull import main as main_pull
+from ribbity.main_build import main as main_build
+
+_testdir = None
+
+
+@pytest.fixture(autouse=True, scope='module')
+def module_setup_teardown():
+    global _testdir
+
+    with tempfile.TemporaryDirectory(prefix='ribbity_') as _testdir:
+        print(f'running tests in temp directory {_testdir}')
+
+        shutil.copy(path_to('..', 'ribbity-test.dmp'), _testdir)
+        shutil.copy(path_to('..', 'config-test.toml'), _testdir)
+
+        cwd = os.getcwd()
+        os.chdir(_testdir)
+
+        print(f'running ribbity.main_build.main')
+        retval = main_build('config-test.toml')
+        assert retval == 0
+
+        # run the tests!
+        try:
+            yield
+        finally:
+            os.chdir(cwd)
+
+    # teardown here, if anything.
+    # note: temp directory should be removed automatically by context mgr.
+    print('done with tests!')
+
 
 thisdir = os.path.dirname(os.path.abspath(__file__))
 def path_to(*p):
@@ -24,6 +60,9 @@ def get_issue_by_number(num):
     issues_list = load_dump('ribbity-test.dmp')
     issue = [ iss for iss in issues_list if iss.number == num][0]
     return issue
+
+
+### tests!
 
 
 def test_pull_issue1_basic():
